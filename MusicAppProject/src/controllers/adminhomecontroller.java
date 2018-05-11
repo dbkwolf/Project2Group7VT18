e97986fa567f1f94db143f3ac0ac0dd0126f6588
Sireeshavundavelli <38317058+Sireeshavundavelli@users.noreply.github.com>
2018-05-11 11:19:35 +0200

    created adminhomepage

---
+++ MusicAppProject/src/controllers/adminhomecontroller.java
@@ -0,0 +1,44 @@
+package controllers;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class adminhomecontroller{
+    public JFXButton btn_Addsongs;
+    public JFXButton btn_Removesongs;
+    public JFXButton btn_editsongs;
+    public JFXButton btn_cancel;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
