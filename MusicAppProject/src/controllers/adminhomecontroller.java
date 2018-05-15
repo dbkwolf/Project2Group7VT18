fb08eb4ccc04d5828910dba70dacaba0e6d3aa00
Sireeshavundavelli <38317058+Sireeshavundavelli@users.noreply.github.com>
2018-05-15 16:40:01 +0200

    created admin controllers and fxml

--- MusicAppProject/src/controllers/adminhomecontroller.java
+++ MusicAppProject/src/controllers/adminhomecontroller.java
@@ -21,24 +21,55 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class adminhomecontroller{
-    public JFXButton btn_Addsongs;
+public class adminhomecontroller {
+    public JFXButton btn_addsongs;
     public JFXButton btn_Removesongs;
     public JFXButton btn_editsongs;
     public JFXButton btn_cancel;
 
+    public void press_btn_addsongs(ActionEvent event) throws Exception {
 
+        Parent registerParent = FXMLLoader.load(getClass().getResource("../scenes/adminaddsongs.fxml"));
+        Scene registerScene = new Scene(registerParent);
 
-
-
-
-
-
-
-
-
-
-
+        //this line gets the stage info
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(registerScene);
+        window.show();
 
 
     }
+
+    public void press_btn_Removesongs(ActionEvent event) throws Exception {
+
+        Parent registerParent = FXMLLoader.load(getClass().getResource("../scenes/adminremovesongs.fxml"));
+        Scene registerScene = new Scene(registerParent);
+
+        //this line gets the stage info
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(registerScene);
+        window.show();
+    }
+
+    public void press_btn_editsongs(ActionEvent event) throws Exception {
+
+        Parent registerParent = FXMLLoader.load(getClass().getResource("../scenes/admineditsongs.fxml"));
+        Scene registerScene = new Scene(registerParent);
+
+        //this line gets the stage info
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(registerScene);
+        window.show();
+    }
+
+    public void press_btn_cancel(ActionEvent event) throws Exception {
+
+        Parent registerParent = FXMLLoader.load(getClass().getResource("../scenes/login.fxml"));
+        Scene registerScene = new Scene(registerParent);
+
+        //this line gets the stage info
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(registerScene);
+        window.show();
+    }
+}
